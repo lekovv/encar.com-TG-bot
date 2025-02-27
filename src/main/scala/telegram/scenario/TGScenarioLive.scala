6ce721a953fc8035e7725d5df5b684da83aabffd
Владимир Леков <lekov137@gmail.com>
2025-02-27 22:35:08 +0300

    step 2 implemented

--- src/main/scala/telegram/scenario/TGScenarioLive.scala
+++ src/main/scala/telegram/scenario/TGScenarioLive.scala
@@ -1,6 +1,7 @@
 package telegram.scenario
 
 import cats.implicits.toFunctorOps
+import com.bot4s.telegram.models.{InlineKeyboardButton, InlineKeyboardMarkup}
 import telegram.TGBotClient
 import zio.interop.catz.asyncInstance
 import zio.{Task, ZIO, ZLayer}
@@ -13,6 +14,18 @@ final case class TGScenarioLive() extends TGScenario[TGBotClient] {
       { _ =>
         bot.reply("Отправьте ссылку на автомобиль с сайта encar.com").void
       }
+    }
+
+    bot.onMessage { implicit msg =>
+      val text = msg.text.getOrElse("")
+      if (text.startsWith("https://fem.encar.com/")) {
+        val cancelButton = InlineKeyboardButton.callbackData("Отменить", "cancel")
+        val markup       = InlineKeyboardMarkup.singleColumn(Seq(cancelButton))
+
+        bot.replyMd("Идет просчет стоимости, пожалуйста, ожидайте...", replyMarkup = Option(markup)).void
+      } else if (text.nonEmpty && text != "/start") {
+        bot.reply("Пожалуйста, отправьте корректную ссылку с сайта encar.com").void
+      } else ZIO.unit
     }
   }
 }
